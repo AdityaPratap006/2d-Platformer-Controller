6c6fec70764bbf889b193ea0e32de287019a2ba3
Aditya Pratap <adityapratap7344@gmail.com>
2023-01-22 02:40:15 +0530

    [ep-3] feat: added ground check and multiple jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Attributes")]
     [SerializeField] float movementSpeed = 10.0f;
     [SerializeField] float jumpForce = 16.0f;
+    [SerializeField] int amountOfJumps = 1;
+
+
+    [Header("Surrounding Check Attributes")]
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask whatIsGround;
+
+    [Header("References")]
+    [SerializeField] Transform groundCheck;
+
 
     float movementInputDirection;
     bool isFacingRight = true;
+    bool isWalking;
+    bool isGrounded;
+    bool canJump;
+    int amountOfJumpsLeft;
 
     Rigidbody2D rb;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -24,11 +42,14 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
     }
 
     private void CheckInput()
@@ -56,6 +77,15 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 
     private void Flip()
@@ -67,6 +97,43 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        if (!canJump) return;
+
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        amountOfJumpsLeft--;
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+
+        if (amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
